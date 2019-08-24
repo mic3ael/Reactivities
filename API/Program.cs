1a580e613b4d417431a2c8044b33ff1566728bb7
Michael Horojanski <mhorojanski@chegg.com>
2019-08-24 11:53:56 +0300

    Init

---
+++ API/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope()){
+               var services = scope.ServiceProvider;
+
+               try{
+                   var context = services.GetRequiredService<DataContext>();
+                   context.Database.Migrate();
+               } catch(Exception exp){
+                   var logger = services.GetRequiredService<ILogger<Program>>();
+                   logger.LogError(exp, "An error occured during migration");
+               }
+            }
+
+            host.Run();
+        }
+        
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
