f5ce5435065b3a00ac74a5d127769035495c868b
Michael Horojanski <mhorojanski@chegg.com>
2019-08-26 09:48:30 +0300

    Add cors support and modules

--- API/Startup.cs
+++ API/Startup.cs
@@ -23,6 +23,15 @@ namespace API
             services.AddDbContext<DataContext>(options => {
                 options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -38,6 +47,8 @@ namespace API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 //app.UseHsts();
             }
+
+            app.UseCors("CorsPolicy");
 
             // app.UseHttpsRedirection();
             app.UseMvc();
