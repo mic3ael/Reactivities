ac45165f4d8ec5365061d7617f47e01a06456f74
Michael Horojanski <mhorojanski@chegg.com>
2019-08-27 22:26:07 +0300

    add handlers for activities

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Application.Activities;
 using Domain;
@@ -17,19 +18,35 @@ namespace API.Controllers {
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> List () {
-            return await _mediator.Send (new List.Query ());
+        public async Task<ActionResult<List<Activity>>> List (CancellationToken ct) {
+            return await _mediator.Send (new List.Query (), ct);
         }
 
         [HttpGet ("{id}")]
-        public async Task<ActionResult<Activity>> Details (Guid id) {
-            var activity = await _mediator.Send (new Details.Query { Id = id });
+        public async Task<ActionResult<Activity>> Details (Guid id, CancellationToken ct) {
+            var activity = await _mediator.Send (new Details.Query { Id = id }, ct);
 
             if (activity == null) {
                 return NotFound ();
             }
 
             return Ok (activity);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Create (Create.Command command) {
+            return await _mediator.Send (command);
+        }
+
+        [HttpPut ("{id}")]
+        public async Task<ActionResult<Unit>> Edit (Guid id, Edit.Command command) {
+            command.Id = id;
+            return await _mediator.Send (command);
+        }
+
+        [HttpDelete ("{id}")]
+        public async Task<ActionResult<Unit>> Delete (Guid id) {
+            return await _mediator.Send (new Delete.Command { Id = id });
         }
     }
 }
--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities {
@@ -12,13 +14,22 @@ namespace Application.Activities {
 
         public class Handler : IRequestHandler<Query, List<Activity>> {
             private readonly DataContext _context;
-            public Handler (DataContext context) {
+            private readonly ILogger<List> _logger;
+            public Handler (DataContext context, ILogger<List> logger) {
+                this._logger = logger;
                 this._context = context;
             }
 
             public async Task<List<Activity>> Handle (Query request, CancellationToken cancellationToken) {
-                var activities = await _context.Activities.ToListAsync();
-                return activities;
+                try {
+                    cancellationToken.ThrowIfCancellationRequested ();
+                    var activities = await _context.Activities.ToListAsync ();
+                    return activities;
+
+                } catch (Exception exp) when (exp is TaskCanceledException) {
+                    _logger.LogInformation (exp, "The request was cancelled");
+                    return null;
+                }
             }
         }
     }
